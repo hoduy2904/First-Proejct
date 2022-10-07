e26dcab0e0410ad2ce71df4d78759c67789702dd
S3CORP\duy.ho <hoduy2904@gmail.com>
2022-10-07 14:19:06 +0700

    First commit by  Source Trê

---
+++ Day4_MVC/Day4_MVC/Content/js/main.js
@@ -0,0 +1,31 @@
+const load=(img)=>{
+    const url=img.getAttribute('lazy-src')
+
+    img.setAttribute('src',url);
+
+}
+
+
+const ready=()=>{
+    if('IntersectionObserver' in window){
+        var lazyImg=document.querySelectorAll('[lazy-src]');
+
+        let Observer=new IntersectionObserver((entries)=>{
+            entries.forEach(entry=>{
+                if(entry.isIntersecting){
+                    console.log(entry);
+                    load(entry.target)
+                }
+            })
+        })
+        
+        lazyImg.forEach(img=>{
+            Observer.observe(img)
+        })
+    }else{
+
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', ready)
---
+++ Day4_MVC/Day4_MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Day4_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day4_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly travelEntities db = new travelEntities();
+        public ActionResult Index()
+        {
+            var travelAbout = db.aboutCountries.ToList();
+            return View(travelAbout);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
